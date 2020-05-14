ec3c7f28b3fec1e29f6196b20bfe66daaa26000a
LiJiaLiEllisLee <31449421+LiJiaLiEllisLee@users.noreply.github.com>
2020-05-15 07:45:29 +0800

    QQ电脑在线状态

---
+++ i/api/qqol/api.php
@@ -0,0 +1,24 @@
+<?php
+    $counter = intval(file_get_contents("counter.dat"));  
+     $_SESSION['#'] = true;  
+     $counter++;  
+     $fp = fopen("counter.dat","w");  
+     fwrite($fp, $counter);  
+     fclose($fp); 
+ ?>
+<?php
+if ($_GET['qq']) {
+$qq = $_GET['qq'];
+$data = file_get_contents("http://webpresence.qq.com/getonline?type=1&$qq:");
+$data || $data = strlen(file_get_contents("http://wpa.qq.com/pa?p=2:$qq:45"));
+if(!$data) { return 0; } 
+ 
+switch((string)$data){
+  case 'online[0]=0;': exit('{"code":"20","msg":"电脑离线"}');return;
+  case 'online[0]=1;': exit('{"code":"21","msg":"电脑在线"}') ;return;
+ 
+} 
+ 
+} 
+return 3;
+?>
