85545c1f6300719ead894495cadd614484964c24
LiJiaLiEllisLee <31449421+LiJiaLiEllisLee@users.noreply.github.com>
2020-05-15 07:37:43 +0800

    信息修改

---
+++ i/website.php
@@ -0,0 +1,39 @@
+<?php
+$website_array=array(
+	"website_author"=>"利熙网络",
+	"website_title"=>"利熙API",
+	"website_url"=>"https://api.liitk.com",
+	"website_keywords"=>"",
+	"website_description"=>"",
+	"website_banner"=>"利熙API平台",
+	"api_url"=>"https://api.liitk.com/i/api/",
+	"api_number"=>"11",
+	"net_name"=>"主页",
+	"net_url"=>"https://www.liitk.com/",
+	"blog_name"=>"博客",
+	"blog_url"=>"https://www.liisk.com/",
+);
+
+$footer_array=array(
+	"admin_email"=>"admin@liitk.com",
+	"footer_title1"=>"关于",
+	"footer_title2"=>"链接",
+	"footer_title3"=>"打赏",
+	"footer_text1"=>"是利熙网络支持并维护的API接口项目，致力于为用户提供稳定、快速的免费API接口服务平台。",
+	"footer_text2"=>"反馈或建议请发送邮件至：admin@liitk.com",
+	"footer_text3"=>"声明：本网站提供免费的接口服务，造成的一切后果与本网站无关！",
+	"footer_img"=>"https://ae01.alicdn.com/kf/H1f86476fd853477cb3cb7de333f3e1d4a.png",
+	"footer_copyright"=>"&copy; 2020 利熙博客 All rights reserved",
+	"footer_icp"=>"",
+);
+
+$links_array=array(
+	"links"=>"<ul class=\"list-unstyled list-inline\">
+				<li><a href=\"https://www.liitk.com/\" target=\"_blank\" onclick=\"_hmt.push(['_trackEvent', 'footer', 'click', 'footer-xviste'])\">网络主页</a></li>
+				<li><a href=\"https://www.liisk.com/\" target=\"_blank\" onclick=\"_hmt.push(['_trackEvent', 'footer', 'click', 'footer-xviste'])\">博客社区</a></li>
+				<li><a href=\"https://shop.liitk.com/\" target=\"_blank\" onclick=\"_hmt.push(['_trackEvent', 'footer', 'click', 'footer-xviste'])\">自助商城</a></li>
+				<li><a href=\"https://www.litv.top/\" target=\"_blank\" onclick=\"_hmt.push(['_trackEvent', 'footer', 'click', 'footer-xviste'])\">全网影视</a></li>
+				<li><a href=\"https://www.liisk.com/site-navigation\" target=\"_blank\" onclick=\"_hmt.push(['_trackEvent', 'footer', 'click', 'footer-xviste'])\">在线工具</a></li>
+			</ul>",
+);
+?>
