704f07d10e29c8a2d19402d5ec3941f16592ecec
LiJiaLiEllisLee <31449421+LiJiaLiEllisLee@users.noreply.github.com>
2020-05-15 07:20:50 +0800

    引导主页

---
+++ index.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html lang="zh-CN">
+<?php require_once("./i/website.php");?>
+ <head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=Edge" />
+  <meta name="viewport" id="viewport" content="width=device-width, initial-scale=1" />
+  <title><?php echo $website_array["website_title"];?> - 专业API提供</title>
+  <meta name="description" content="<?php echo $website_array["website_title"];?>免费提供Api服务" />
+  <meta name="keywords" content="<?php echo $website_array["website_title"];?>免费提供Api服务" />
+  <link rel="shortcut icon" href="favicon.ico" />
+  <link rel="stylesheet" type="text/css" href="css/style.css" />
+  <script src="js/3.3.1-jquery.min.js"></script>
+  <script src="js/page.js?ver=1.1"></script>
+ </head>
+ <body>
+  <div class="background">
+  </div>
+  <nav>
+   <!-- <a href="https://www.liisk.com/" target="_blank">Blog</a> -->
+   <a class="active">Home</a>
+   <a title="时间的颜色OwO" class="time">#Loading</a>
+  </nav>
+  <div class="main">
+   <div class="ch intro">
+    <div class="el">
+     <span></span>
+     <span></span>
+     <span></span>
+     <span></span>
+    </div>
+    <div class="hello">
+     <h1 id="slogan">思考中…… </h1>
+     <h2>
+      <div class="circle">
+       <span></span>利熙网络
+       <span></span>免费提供
+       <span></span>API服务
+      </div><!-- 利熙网络免费提供API服务 --></h2>
+    </div>
+   </div>
+   <div class="ch about">
+    <div class="container">
+     <h2 class="chtitle">你好，<span>这里是利熙API！</span>滴滴滴！</h2>
+     <div class="clear">
+   <div class="introduct">
+	<!-- <p>欢迎你来到本API页面！</p>
+	<p>1.利用NGINX重定向规则 把PHP后缀屏蔽掉</p>
+	<p>2.更新API获取速度</p> -->
+	<p>站长太懒，什么也没写。<p>也不知道写点什么，祝你生活愉快😙！</p>
+	</div>
+      <ul class="skill">
+       <li> <p>可靠性</p>
+        <div class="progress">
+         <div style="width:98%;"></div>
+        </div> </li>
+       <li> <p>可调用</p>
+        <div class="progress">
+         <div style="width:95%;"></div>
+        </div> </li>
+       <li> <p>速度性</p>
+        <div class="progress">
+         <div style="width:94%;"></div>
+        </div> </li>
+       <li> <p>兼容性</p>
+        <div class="progress">
+         <div style="width:95%;"></div>
+        </div> </li>
+       <li> <p>安全性</p>
+        <div class="progress">
+         <div style="width:97%;"></div>
+        </div> </li>
+      </ul>
+     </div>
+    </div>
+   </div>
+   <div class="find ch">
+    <div class="container links">
+     <h2 class="chtitle">利熙API<span> Center</span></h2>
+     <div class="clear">
+      <a href="i">
+       <div class="item">
+        <div class="bg" style="background-color: #28A9E0"></div>
+        <div class="inner">
+         <span>接口列表</span>
+        </div>
+       </div></a>
+      <a href="https://www.liitk.com/" target="_blank">
+       <div class="item">
+        <div class="bg" style="background-color: #E6162D"></div>
+        <div class="inner">
+         <span>网络主页</span>
+        </div>
+       </div></a>
+      <a href="https://www.liisk.com/" target="_blank">
+       <div class="item">
+        <div class="bg" style="background-color: #F09199"></div>
+        <div class="inner">
+         <span>博客社区</span>
+        </div>
+       </div></a>
+      <a href="https://shop.liitk.com/" target="_blank">
+       <div class="item">
+        <div class="bg" style="background-color: #b600ff"></div>
+        <div class="inner">
+         <span>自助商城</span>
+        </div>
+       </div></a>
+ <!--     <a href="/" target="_blank">
+       <div class="item">
+        <div class="bg" style="background-color: #00FFFF"></div>
+        <div class="inner">
+         <span>待添加</span>
+        </div>
+       </div></a>
+      <a href="/" target="_blank">
+       <div class="item">
+        <div class="bg" style="background-color: #010504"></div>
+        <div class="inner">
+         <span>待添加</span>
+        </div>
+       </div></a>
+       <a href="/" target="_blank">
+       <div class="item">
+        <div class="bg" style="background-color: #7B68EE"></div>
+        <div class="inner">
+         <span>待添加</span>
+        </div>
+       </div></a>
+      <a href="/" target="_blank">
+       <div class="item">
+        <div class="bg" style="background-color: #0000FF"></div>
+        <div class="inner">
+         <span>待添加</span>
+        </div>
+       </div></a> -->
+     </div>
+    </div>
+   </div>
+<!--    <div class="gate ch">
+    <div class="container links">
+     <h2 class="chtitle">由以下提供<span>赞助</span></h2>
+     <div class="clear">
+      <a href="#" target="_blank">
+       <div class="item">
+        <div class="avatar">
+         <img src="/" />
+        </div>
+        <div class="inner">
+         <h5>昵 称</h5>
+         <p>简 约 介 绍</p>
+        </div>
+       </div></a>
+
+     </div>
+    </div>
+   </div>  -->
+   <div class="footer ch">
+    <div class="container">
+     <h3>利熙<a href="/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="1657667f565873626179647d">[email&nbsp;protected]</a></h3>
+     <p class="c">© 2020 <a href="#" rel="nofollow" target="_blank">利熙网络</a> All rights reserved<!-- |<a href="#">粤ICP备xxxxxxxx号</a></p> -->
+	 <p><script data-cfasync="false" src="js/email-decode.min.js"></script></p>
+    </div>
+   </div>
+  </div>
+ </body>
+</html>
