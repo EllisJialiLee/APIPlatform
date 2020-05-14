342afbdad65420959cec2036ef1d11b8434c9cf9
LiJiaLiEllisLee <31449421+LiJiaLiEllisLee@users.noreply.github.com>
2020-05-15 07:47:06 +0800

    随机一言语录

---
+++ i/api/yyan/index-back.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="zh-CN">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="description" content="">
+<meta name="keywords" content="">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="theme-color" content="#fd4b5c">
+<title>随机语录-免费API-利熙网络</title>
+<script src="https://api.liecnk.com/yan/api.php?encode=js"></script>
+</head>
+<style type="text/css">
+h3:hover {box-shadow:0px 0px 8px #D1D1D1;}
+</style>
+<div style="box-shadow: 5px 5px 25px 0 rgba(46,61,73,.2);border-radius:15px;font-size:13px;width:950px;font-family:微软雅黑,'Helvetica Neue',Arial,sans-serif;margin:10px auto 0px;border:0px solid #eee;max-width:100%;">
+    <div style="width:100%;background-color: #3174ed;background-image: linear-gradient(90deg, #3174ed 0%, #FA8BFF 35%, #3fd9fb 88%);color:#FFFFFF;border-radius:15px 15px 0 0;">
+        <h2 style="font-size:15px;word-break:break-all;padding:20px 32px;margin:0;text-align:center">随机语录-免费API-利熙网络</h2>
+    </div>
+    <div style="margin:0px auto;width:90%">
+        <h3 style="-webkit-transition: all .2s cubic-bezier(0, 0, 0, 0.48);-moz-transition: all .2s ease;border:.0625rem solid #fafafa;background:#fafafa repeating-linear-gradient(-45deg,#fff,#fff 1.125rem,transparent 1.125rem,transparent 2.25rem);margin:15px 0px;padding:20px;border-radius:5px;font-size:14px;color:#333;">
+		# 随机语录API请求方式 #
+		<ul>
+		<li>Method: Get</li>
+		</ul>
+		<hr>
+		# 请求地址 #<br/>
+		<a style="color:#fd4b5c;text-decoration:none;">https://api.liecnk.com/yan/api.php</a><br/>
+		# 参数 #<br/>
+		<li>charset//字符编码（gbk/utf-8）</li>
+        <li>encode//返回类型（text/js/json）</li>
+		<hr>
+		# 返回数据 #<br/>
+		<div style="width:100%;background-color: #000;background-image: linear-gradient(90deg, #6694ed 0%,);color:#FFFFFF;border-radius:5px;word-wrap:break-word;word-break:break-all;">
+             js输出：function binduyan(){document.write('<script>binduyan()</script>');}
+        </div>
+		<hr>
+		# 备注 #<br/>
+		html调用举例:&lt;iframe src="https://api.liecnk.com/yan/api.php">&lt;/iframe><br/>
+		js调用举例:&lt;script src="https://api.liecnk.com/yan/api.php?encode=js">&lt;/script><br/>
+        js输出举例:&lt;div>&lt;script>binduyan()&lt;/script>&lt;/div>
+		<hr>
+		# 示例 #<br/>
+		<a style="color:#fd4b5c;text-decoration:none；" target="_blank">https://api.liecnk.com/yan/api.php?charset=utf-8</a><br>
+        <a style="color:#fd4b5c;text-decoration:none；" target="_blank">https://api.liecnk.com/yan/api.php?encode=js</a>
+		</h3>
+          </div>
+</div>
+</html>
